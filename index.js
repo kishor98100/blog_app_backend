0ef82569aaaa90a0aaaa06822b3876ca047b431b
Kishor Mainali <mainali1522kishor@gmail.com>
2020-12-22 20:31:42 +0545

    mongo db changes

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then((connected) => console.log("MongoDb Connected"))
-    .catch(err => console.log(err));
+// mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+//     .then((connected) => console.log("MongoDb Connected"))
+//     .catch(err => console.log(err));
 
 //middleware and routes
 
