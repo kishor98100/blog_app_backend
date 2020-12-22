3b13bdb8b75d651a2cc72be97973dac69ed804cf
Kishor Mainali <mainali1522kishor@gmail.com>
2020-12-22 21:46:21 +0545

    changes

--- utils/multer.js
+++ utils/multer.js
@@ -4,9 +4,7 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "./uploads");
     },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "_" + req.file.filename);
-    }
+
 });
 const fileFilter = (req, file, cb) => {
     if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
